53bc6f2bb3ad6b5b29116cc1473c49520135af45
Jisun696 <istiakkashem35@gmail.com>
2024-04-27 18:05:54 +0600

    started adding auth-providers and components

---
+++ src/components/Header.jsx
@@ -0,0 +1,92 @@
+import { useContext, useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { AuthContext } from '../providers/AuthProvider';
+import defaultPlaceholder from '../assets/Default_Placeholder.webp';
+
+const Header = () => {
+  const [clicked, setClicked] = useState(false);
+
+  const { user, loading } = useContext(AuthContext);
+
+  const userLogout = () => {};
+  return (
+    <header className="container mx-auto font-source">
+      <div className="navbar bg-base-100">
+        <div className="navbar-start items-center">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" onClick={() => setClicked(!clicked)} className="btn btn-ghost lg:hidden p-1">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+              </svg>
+            </div>
+            {clicked && (
+              <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-20 p-2 shadow bg-base-100 rounded-box w-52 text-neutral-600 font-bold">
+                <li className="hover:text-neutral-900">
+                  <NavLink to="/">Home</NavLink>
+                </li>
+                <li className="hover:text-neutral-900">
+                  <NavLink to="/update-profile">Update Profile</NavLink>
+                </li>
+                <li className="hover:text-neutral-900">
+                  <NavLink to="/wishlist">Wishlist</NavLink>
+                </li>
+                {user && (
+                  <li className="hover:text-neutral-900">
+                    <NavLink to="/user-profile">User Profile</NavLink>
+                  </li>
+                )}
+              </ul>
+            )}
+          </div>
+          <NavLink
+            to="/"
+            className="btn btn-ghost text-2xl sm:text-3xl text-transparent bg-gradient-to-br from-slate-950 via-slate-600 to-slate-950 bg-clip-text font-black font-kufam tracking-tight max-sm:mt-1 max-sm:-ml-3">
+            HOMELOCUS
+          </NavLink>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 text-neutral-600 text-base font-bold gap-2 font-kufam">
+            <li className="hover:text-neutral-900">
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li className="hover:text-neutral-900">
+              <NavLink to="/update-profile">Update Profile</NavLink>
+            </li>
+            <li className="hover:text-neutral-900">
+              <NavLink to="/wishlist">Wishlist</NavLink>
+            </li>
+            {user && (
+              <li className="hover:text-neutral-900">
+                <NavLink to="/user-profile">User Profile</NavLink>
+              </li>
+            )}
+          </ul>
+        </div>
+        <div className="navbar-end">
+          {loading && <span className="loading loading-infinity loading-lg mr-4"></span>}
+          {user ? (
+            <>
+              <div className="tooltip tooltip-left cursor-pointer -mb-1" data-tip={user.displayName}>
+                <div className="avatar">
+                  <div className="w-12 rounded-full mr-2">
+                    <img src={user.photoURL || defaultPlaceholder} referrerPolicy="no-referrer" />
+                  </div>
+                </div>
+              </div>
+
+              <button onClick={userLogout} className="btn btn-neutral lg:px-6 font-bold lg:text-lg">
+                Sign Out
+              </button>
+            </>
+          ) : (
+            <Link to="/login" className="btn btn-neutral lg:px-6 font-bold lg:text-lg">
+              Sign In
+            </Link>
+          )}
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,16 @@
+import { createContext, useState } from 'react';
+import PropTypes from 'prop-types';
+
+export const AuthContext = createContext(null);
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const authInfo = { user };
+  return <AuthContext.Provider value={authInfo}></AuthContext.Provider>;
+};
+
+export default AuthProvider;
+
+AuthProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
