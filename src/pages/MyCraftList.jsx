64ac71bb5b16c9e3f7f89dfe6d1d4aebffb8cc85
Jisun696 <istiakkashem35@gmail.com>
2024-04-30 02:40:03 +0600

    started building myCraftList page

--- src/pages/MyCraftList.jsx
+++ src/pages/MyCraftList.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import ArtCard from '../components/ArtCard';
 
 const MyCraftList = () => {
   return (
-    <div>
-      <h1>My Art & Craft List</h1>
+    <div className="container mx-auto font-gsans">
+      <header>
+        <h1 className="text-4xl font-bold text-center mb-2">Your Craft Items</h1>
+        <p className="text-center text-xl text-base-content font-medium mb-4">Find the best craft and art items you have added</p>
+      </header>
+      <main>{/* <ArtCard /> */}</main>
     </div>
   );
 };
