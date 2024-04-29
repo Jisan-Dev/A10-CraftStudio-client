38521f1950098036e7ac71792f35c315162ff724
Jisun696 <istiakkashem35@gmail.com>
2024-04-30 05:20:25 +0600

    added filter by customization

--- src/pages/MyCraftList.jsx
+++ src/pages/MyCraftList.jsx
@@ -5,6 +5,7 @@ import { AuthContext } from '../providers/AuthProvider';
 const MyCraftList = () => {
   const [allCrafts, setAllCrafts] = useState([]);
   const { user } = useContext(AuthContext);
+  const [customization, setCustomization] = useState('');
   useEffect(() => {
     fetch(`http://localhost:5000/allProducts/${user?.email}`)
       .then((res) => res.json())
@@ -12,12 +13,40 @@ const MyCraftList = () => {
   }, [user]);
   console.log(allCrafts);
 
+  useEffect(() => {
+    if (customization === '') {
+      return;
+    }
+    if (customization === 'yes') {
+      fetch(`http://localhost:5000/allProducts/${user?.email}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setAllCrafts(data.filter((craft) => craft.customization === 'yes'));
+        });
+      return;
+    } else if (customization === 'no') {
+      fetch(`http://localhost:5000/allProducts/${user?.email}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setAllCrafts(data.filter((craft) => craft.customization === 'no'));
+        });
+      return;
+    }
+  }, [customization]);
+
   return (
     <div className="container mx-auto font-gsans">
       <header>
         <h1 className="text-4xl font-bold text-center mb-2">Your Craft Items</h1>
         <p className="text-center text-xl text-base-content font-medium mb-4">Find the best craft and art items you have added</p>
       </header>
+      <div className="my-3">
+        <select onChange={(e) => setCustomization(e.target.value)} name="customization" id="customization" className="w-52 shadow-md p-3 rounded-md">
+          <option value="">filter by customization</option>
+          <option value="yes">yes</option>
+          <option value="no">no</option>
+        </select>
+      </div>
       <main className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         {allCrafts &&
           allCrafts.map((craft) => {
